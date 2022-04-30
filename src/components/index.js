726f33b6a3b05869e2505694cc8ae1fa52966b58
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2022-04-30 16:48:24 -0300

    Importing new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Footer from './Footer'
 import Header from './Header'
+import Bar from './Bar'
+import InfoBox from './InfoBox'
 
-export { Footer, Header }
+export { Footer, Header, Bar, InfoBox }
