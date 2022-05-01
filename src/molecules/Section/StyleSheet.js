be220b7670711d282618797b126c27b5593bdefc
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2022-05-01 03:46:02 -0300

    Refactored style

--- src/molecules/Section/StyleSheet.js
+++ src/molecules/Section/StyleSheet.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-    padding: 40px;
+    padding: 48px 0 0 48px;
 `
 
 export const Row = styled.div`
-    flexDirection: row;
+    flex-direction: row;
     display: flex;
 `
 
 export const InfoBoxWrapper = styled.div`
-    margin-left: 150px;
-    margin-right: 200px;
+    margin-left: 20px;
 `
