9e4e62fb69c2e0e00862cbb4173f65d85ba65cff
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2025-03-08 15:56:59 -0300

    update icons

--- src/components/organisms/Overview.tsx
+++ src/components/organisms/Overview.tsx
@@ -6,7 +6,7 @@ export function Overview() {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="avatar">
           <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            <img src="src/assets/avatar-gather.png" />
           </div>
         </div>
         <div>
