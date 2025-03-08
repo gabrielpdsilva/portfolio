279791ea6197a5a995e849a5b880808fc5b42d3b
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2025-03-08 14:35:21 -0300

    add github and so icons

--- src/components/icons/constants.ts
+++ src/components/icons/constants.ts
@@ -1,2 +1,3 @@
 export const FLAG_ICON_SIZE = 28;
 export const TEC_ICON_SIZE = 40;
+export const SOCIAL_MEDIAS_ICON_SIZE = 28;
--- src/components/molecules/Footer/index.tsx
+++ src/components/molecules/Footer/index.tsx
@@ -1,11 +1,31 @@
+import { openInNewTab } from '../../../shortcuts';
+import { Github } from '../../icons/social-medias/Github';
+import { StackOverflow } from '../../icons/social-medias/StackOverflow';
+
 export function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
       <aside>
-        <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by
-          Gabriel
-        </p>
+        <p>Copyright © {currentYear} - All right reserved by Gabriel</p>
+        <div className="flex">
+          <div
+            className="hover:cursor-pointer"
+            onClick={() => openInNewTab('https://github.com/gabrielpdsilva')}
+          >
+            <Github />
+          </div>
+          <div
+            className="hover:cursor-pointer"
+            onClick={() =>
+              openInNewTab(
+                'https://stackoverflow.com/users/12829458/gabrielpdsilva'
+              )
+            }
+          >
+            <StackOverflow />
+          </div>
+        </div>
       </aside>
     </footer>
   );
