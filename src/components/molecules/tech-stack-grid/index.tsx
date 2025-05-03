f6df47f94658767f59dce6637eb9bb0c9bcc6f98
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2025-05-03 18:26:40 -0300

    update names

--- src/components/molecules/tech-stack-grid/index.tsx
+++ src/components/molecules/tech-stack-grid/index.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next'
 import { ICON_TOOLTIP_CLASSNAME } from '../../../constants/styles'
-import { Mobile } from '../../icons/stack/mobile'
-import { Monitor } from '../../icons/stack/monitor'
-import { Database } from '../../icons/stack/database'
+import { Mobile } from '../../icons/stack/mb'
+import { Monitor } from '../../icons/stack/mt'
+import { Database } from '../../icons/stack/db'
 
 export function TechStackGrid() {
   const { t } = useTranslation()
