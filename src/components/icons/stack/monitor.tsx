d50cd790f45d0df664e82b503177edbf231e5d18
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2025-05-03 19:30:39 -0300

    fix console errors

--- src/components/icons/stack/monitor.tsx
+++ src/components/icons/stack/monitor.tsx
@@ -10,9 +10,9 @@ export function Monitor() {
       viewBox="0 0 24 24"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M5 3a2 2 0 0 0-2 2v5h18V5a2 2 0 0 0-2-2H5ZM3 14v-2h18v2a2 2 0 0 1-2 2h-6v3h2a1 1 0 1 1 0 2H9a1 1 0 1 1 0-2h2v-3H5a2 2 0 0 1-2-2Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   )
