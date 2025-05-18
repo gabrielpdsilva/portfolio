4bd94747306e76d8014f601ebd8ca6ba0a9914e5
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2025-05-17 21:05:58 -0300

    split code

--- src/components/organisms/overview/index.tsx
+++ src/components/organisms/overview/index.tsx
@@ -2,7 +2,7 @@ import { useContent } from '../../../hooks/use-content'
 import { ICON_TOOLTIP_CLASSNAME } from '../../../constants/styles'
 
 export function Overview() {
-  const { overview, technologies, stacks } = useContent()
+  const { overview } = useContent()
   return (
     <div className="card w-fit border-1 border-base-content">
       <div className="card-body">
@@ -23,32 +23,38 @@ export function Overview() {
             <p className="italic">{overview.detail}</p>
           </div>
         </div>
-        <div className="space-y-1">
-          <div className="flex justify-center space-x-1">
-            {stacks.map(({ name, Icon }) => (
-              <div
-                key={name}
-                className={ICON_TOOLTIP_CLASSNAME}
-                data-tip={name}
-              >
-                <Icon />
-              </div>
-            ))}
-          </div>
 
-          <div className="place-items-center grid grid-cols-5 md:grid-cols-10">
-            {technologies.map(({ name, Icon }) => (
-              <div
-                key={name}
-                data-tip={name}
-                className={ICON_TOOLTIP_CLASSNAME}
-              >
-                <Icon />
-              </div>
-            ))}
-          </div>
+        <div className="space-y-1">
+          <Stacks />
+          <Techs />
         </div>
       </div>
+    </div>
+  )
+}
+
+function Stacks() {
+  const { stacks } = useContent()
+  return (
+    <div className="flex justify-center space-x-1">
+      {stacks.map(({ name, Icon }) => (
+        <div key={name} className={ICON_TOOLTIP_CLASSNAME} data-tip={name}>
+          <Icon />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function Techs() {
+  const { technologies } = useContent()
+  return (
+    <div className="place-items-center grid grid-cols-5 md:grid-cols-10">
+      {technologies.map(({ name, Icon }) => (
+        <div key={name} data-tip={name} className={ICON_TOOLTIP_CLASSNAME}>
+          <Icon />
+        </div>
+      ))}
     </div>
   )
 }
