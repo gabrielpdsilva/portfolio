65684d304e709882e54d4651e29b64f7cfece585
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2025-05-18 17:17:18 -0300

    fallback improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react'
 import { Portfolio } from './pages/portfolio'
+import { Fallback } from './components/atoms/fallback'
 
 function App() {
   return (
-    <Suspense fallback="loading">
+    <Suspense fallback={<Fallback />}>
       <Portfolio />
     </Suspense>
   )
---
+++ src/components/atoms/fallback/index.tsx
@@ -0,0 +1,7 @@
+export function Fallback() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <span className="loading loading-spinner text-primary" />
+    </div>
+  )
+}
