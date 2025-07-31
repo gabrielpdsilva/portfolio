aaefbc18929bed45eef56275e6b0dfc638e23828
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2025-07-31 18:27:03 -0300

    add more theme options

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,41 @@
 export type Language = 'pt-BR' | 'en'
 
-export type Theme = 'silk' | 'sunset'
+export const themes = [
+  'light',
+  'dark',
+  'cupcake',
+  'bumblebee',
+  'emerald',
+  'corporate',
+  'synthwave',
+  'retro',
+  'cyberpunk',
+  'valentine',
+  'halloween',
+  'garden',
+  'forest',
+  'aqua',
+  'lofi',
+  'pastel',
+  'fantasy',
+  'wireframe',
+  'black',
+  'luxury',
+  'dracula',
+  'cmyk',
+  'autumn',
+  'business',
+  'acid',
+  'lemonade',
+  'night',
+  'coffee',
+  'winter',
+  'dim',
+  'nord',
+  'sunset',
+  'caramellatte',
+  'abyss',
+  'silk',
+] as const
+
+export type Theme = (typeof themes)[number]
