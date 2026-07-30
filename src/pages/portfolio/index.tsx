c5771e4425e048bde736b624d216aaf14c403e3a
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2026-07-30 17:39:20 -0300

    refactor: keep components generic

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,30 +1,49 @@
 import { Footer } from '../../components/organisms/footer'
 import { Section } from '../../components/molecules/section'
-import { Principles } from '../../components/organisms/principles'
-import { Overview } from '../../components/organisms/overview'
-import { AcademicTrajectory } from '../../components/organisms/academic-trajectory'
+import { SimpleCardsGrid } from '../../components/organisms/simple-cards-grid'
+import { BoxOverview } from '../../components/organisms/box-overview'
 import { Header } from '../../components/organisms/header'
-import { Highlights } from '../../components/organisms/highlights'
+import { DetailedCardsGrid } from '../../components/organisms/detailed-cards-grid'
 import { useContent } from '../../hooks/use-content'
+import { Timeline } from '../../components/organisms/timeline'
 
 export function Portfolio() {
-  const { highlights, principles, timeline } = useContent()
+  const {
+    overview,
+    stacks,
+    technologies,
+    highlights,
+    principles,
+    professional,
+    academic,
+  } = useContent()
   return (
     <div>
       <Header />
       <div className="hero bg-base-200 min-h-screen">
         <div className="space-y-8 p-4">
           <div className="flex justify-center">
-            <Overview />
+            <BoxOverview
+              title={overview.title}
+              subtitle={overview.subtitle}
+              detail={overview.detail}
+              primaryIcons={stacks}
+              secondaryIcons={technologies}
+            />
           </div>
           <Section title={highlights.title}>
-            <Highlights />
+            <DetailedCardsGrid items={highlights.items} />
           </Section>
+
           <Section title={principles.title}>
-            <Principles />
+            <SimpleCardsGrid items={principles.items} />
           </Section>
-          <Section title={timeline.title}>
-            <AcademicTrajectory />
+          <Section title={professional.title}>
+            <Timeline items={professional.items} />
+          </Section>
+          <div className="divider" />
+          <Section title={academic.title}>
+            <Timeline items={academic.items} />
           </Section>
         </div>
       </div>
