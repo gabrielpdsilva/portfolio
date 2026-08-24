f6c56c48c0097c124ac5e9d61dc9346c621927df
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2026-08-24 17:58:30 -0300

    feat: add transition effect on scroll

--- src/components/molecules/section/index.tsx
+++ src/components/molecules/section/index.tsx
@@ -1,10 +1,18 @@
-import { ReactNode } from 'react'
+import { useIsVisible } from '@/hooks/use-is-visible'
+import { ReactNode, useRef } from 'react'
 
 export function Section({ title, children }: SectionProps) {
+  const ref = useRef<HTMLDivElement | null>(null)
+  const isSectionVisible = useIsVisible(ref)
   return (
-    <div className="space-y-3">
-      <p className="font-bold text-2xl text-center">{title}</p>
-      {children}
+    <div
+      ref={ref}
+      className={`transition-opacity ease-in duration-700 ${isSectionVisible ? 'opacity-100' : 'opacity-0'}`}
+    >
+      <div className="space-y-3">
+        <p className="font-bold text-2xl text-center">{title}</p>
+        {children}
+      </div>
     </div>
   )
 }
