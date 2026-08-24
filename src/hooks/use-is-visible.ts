da32da6175562246dd560a6194a5e6ba9e1a1e15
gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2026-08-24 18:04:44 -0300

    chore: add medium url

--- src/hooks/use-is-visible.ts
+++ src/hooks/use-is-visible.ts
@@ -1,5 +1,7 @@
 import { RefObject, useEffect, useState } from 'react'
 
+// Thanks to:
+// https://medium.com/@jacobvejlinjensen/
 export function useIsVisible(ref: RefObject<HTMLDivElement | null>) {
   const [isIntersecting, setIntersecting] = useState(false)
 
